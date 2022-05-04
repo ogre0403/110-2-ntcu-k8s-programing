92bf72bf12c3928212f3d90bb78b78f635cc3c99
Jimmy Chuang <ogre0403@gmail.com>
2022-05-04 20:34:14 +0800

    add in-cluster example

---
+++ Makefile
@@ -0,0 +1,19 @@
+IMG ?= ogre0403/incluster
+TAG ?= latest
+
+
+run-client:
+	go run ./cmd/client
+
+run-in-cluster:
+	go run ./cmd/incluster -outside-cluster
+
+build-in-cluster-image:
+	docker build -t $(IMG):$(TAG) .
+
+load-in-cluster-image:
+	kind load docker-image $(IMG):$(TAG)
+
+run-informer:
+	go run ./cmd/informer
+
--- cmd/incluster/main.go
+++ cmd/incluster/main.go
@@ -2,48 +2,120 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
-	"k8s.io/apimachinery/pkg/api/errors"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"os/signal"
+	"path"
+	"syscall"
 	"time"
 )
 
+var (
+	namespace = "default"
+)
+
 func main() {
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	// creates the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-	for {
-		// get pods in all the namespaces by omitting namespace
-		// Or specify namespace to get pods in particular namespace
-		pods, err := clientset.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{})
+	outsideCluster := flag.Bool("outside-cluster", false, "set to true when run out of cluster. (default: false)")
+	flag.Parse()
+
+	var clientset *kubernetes.Clientset
+	if *outsideCluster {
+		// creates the out-cluster config
+		home, err := os.UserHomeDir()
+		if err != nil {
+			panic(err)
+		}
+		config, err := clientcmd.BuildConfigFromFlags("", path.Join(home, ".kube/config"))
 		if err != nil {
 			panic(err.Error())
 		}
-		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
-
-		// Examples for error handling:
-		// - Use helper functions e.g. errors.IsNotFound()
-		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
-		_, err = clientset.CoreV1().Pods("default").Get(context.Background(), "example-xxxxx", metav1.GetOptions{})
-		if errors.IsNotFound(err) {
-			fmt.Printf("Pod example-xxxxx not found in default namespace\n")
-		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-			fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-		} else if err != nil {
+		// creates the clientset
+		clientset, err = kubernetes.NewForConfig(config)
+		if err != nil {
 			panic(err.Error())
-		} else {
-			fmt.Printf("Found example-xxxxx pod in default namespace\n")
 		}
-
-		time.Sleep(10 * time.Second)
+	} else {
+		// creates the in-cluster config
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			panic(err.Error())
+		}
+		// creates the clientset
+		clientset, err = kubernetes.NewForConfig(config)
+		if err != nil {
+			panic(err.Error())
+		}
 	}
+
+	cm := createConfigMap(clientset)
+
+	go func() {
+		for {
+			read, err := clientset.
+				CoreV1().
+				ConfigMaps(namespace).
+				Get(
+					context.Background(),
+					cm.GetName(),
+					metav1.GetOptions{},
+				)
+			if err != nil {
+				panic(err.Error())
+			}
+
+			fmt.Printf("Read ConfigMap %s/%s, value is %s\n", namespace, read.GetName(), read.Data["foo"])
+			time.Sleep(5 * time.Second)
+		}
+	}()
+
+	fmt.Println("Waiting for Kill Signal...")
+	var stopChan = make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-stopChan
+
+	fmt.Printf("Delete ConfigMap %s/%s ", namespace, cm.GetName())
+	deleteConfigMap(clientset, cm)
+}
+
+func createConfigMap(client kubernetes.Interface) *corev1.ConfigMap {
+	cm := &corev1.ConfigMap{Data: map[string]string{"foo": "bar"}}
+	cm.Namespace = namespace
+	cm.GenerateName = "informer-typed-simple-"
+
+	cm, err := client.
+		CoreV1().
+		ConfigMaps(namespace).
+		Create(
+			context.Background(),
+			cm,
+			metav1.CreateOptions{},
+		)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Created ConfigMap %s/%s\n", cm.GetNamespace(), cm.GetName())
+	return cm
+}
+
+func deleteConfigMap(client kubernetes.Interface, cm *corev1.ConfigMap) {
+	err := client.
+		CoreV1().
+		ConfigMaps(cm.GetNamespace()).
+		Delete(
+			context.Background(),
+			cm.GetName(),
+			metav1.DeleteOptions{},
+		)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Printf("Deleted ConfigMap %s/%s\n", cm.GetNamespace(), cm.GetName())
 }
