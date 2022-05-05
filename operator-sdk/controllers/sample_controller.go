b89134c47c82db5d9ed32821591a4d78f22d9e9f
Jimmy Chuang <ogre0403@gmail.com>
2022-05-05 15:41:37 +0800

    add operator-sdk example

--- operator-sdk/controllers/sample_controller.go
+++ operator-sdk/controllers/sample_controller.go
@@ -18,10 +18,13 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -52,11 +55,117 @@ type SampleReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.11.0/pkg/reconcile
 func (r *SampleReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
 	// TODO(user): your logic here
+	// Fetch the Memcached instance
+	sample := &samplev1alpha1.Sample{}
+	err := r.Get(ctx, req.NamespacedName, sample)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("Sample resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get sample")
+		return ctrl.Result{}, err
+	}
+
+	foundJob := &batchv1.Job{}
+	err = r.Get(ctx, types.NamespacedName{Name: sample.Name, Namespace: sample.Namespace}, foundJob)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		dep := r.newJob(sample)
+		log.Info("Creating a new Job", "Job.Namespace", dep.Namespace, "Job.Name", dep.Name)
+		err = r.Create(ctx, dep)
+		if err != nil {
+			log.Error(err, "Failed to create new Job", "Job.Namespace", dep.Namespace, "Job.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+		// Deployment created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get Job")
+		return ctrl.Result{}, err
+	}
+
+	foundCM := &corev1.ConfigMap{}
+	err = r.Get(ctx, types.NamespacedName{Name: sample.Name, Namespace: sample.Namespace}, foundCM)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		dep := r.newConfigMap(sample)
+		log.Info("Creating a new ConfigMap", "ConfigMap.Namespace", dep.Namespace, "ConfigMap.Name", dep.Name)
+		err = r.Create(ctx, dep)
+		if err != nil {
+			log.Error(err, "Failed to create new ConfigMap", "ConfigMap.Namespace", dep.Namespace, "ConfigMap.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+		// Deployment created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get ConfigMap")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+func (r *SampleReconciler) newConfigMap(s *samplev1alpha1.Sample) *corev1.ConfigMap {
+
+	cm := &corev1.ConfigMap{
+		Data: map[string]string{s.Spec.Key: s.Spec.Value},
+	}
+	cm.Name = s.Name
+	cm.Namespace = s.Namespace
+
+	ctrl.SetControllerReference(s, cm, r.Scheme)
+	return cm
+}
+
+func (r *SampleReconciler) newJob(s *samplev1alpha1.Sample) *batchv1.Job {
+	job := &batchv1.Job{
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: s.Name,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image:   s.Spec.Image,
+						Name:    s.Name,
+						Command: []string{"cat", fmt.Sprintf("/tmp/cm/%s", s.Spec.Key)},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      s.Name,
+								MountPath: "/tmp/cm",
+							},
+						},
+					}},
+					Volumes: []corev1.Volume{
+						{
+							Name: s.Name,
+							VolumeSource: corev1.VolumeSource{
+								ConfigMap: &corev1.ConfigMapVolumeSource{
+									LocalObjectReference: corev1.LocalObjectReference{
+										Name: s.Name,
+									},
+								},
+							},
+						},
+					},
+					RestartPolicy: corev1.RestartPolicyNever,
+				},
+			},
+		},
+	}
+	job.Name = s.Name
+	job.Namespace = s.Namespace
+
+	ctrl.SetControllerReference(s, job, r.Scheme)
+	return job
 }
 
 // SetupWithManager sets up the controller with the Manager.
